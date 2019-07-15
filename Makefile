5b373dec1d1cc67a92c8078c7c633efb8e2c8061
morimolymoly <morimolymoly@gmail.com>
2019-07-15 23:13:53 +0900

    vga

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
 	bootimage build --target x86_64-weed.json
 
-run:
+run: build
 	qemu-system-x86_64 -drive format=raw,file=target/x86_64-weed/debug/bootimage-weed-os.bin
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,20 @@
 #![no_std]
 #![no_main]
 
-// println!マクロはつかえないこれはstdライブラリの範疇(stdoutはOSが用意するよね)
-/*
-fn main() {
-}*/
+use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello, World!";
+mod vga_buffer;
 
-// LLVMによりLinuxっぽい規則で
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("hello {}", "weeed");
+    panic!("owata");
     loop {}
 }
 
-use core::panic::PanicInfo;
-
-// panicハンドラはpanicが起こったときにrustが呼び出す
-// 返り値の!は終わらない関数であることを意味する
+/// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop{}
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
 }
